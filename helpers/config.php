ee7f0a9cdfa466281fa4c20d5aad6ded940bfbb2
Daniel Doyle <tr33m4n@googlemail.com>
2019-07-03 15:39:56 +0100

    Add scope to config provider helper

--- helpers/config.php
+++ helpers/config.php
@@ -9,18 +9,19 @@ use DanielDoyle\HappyUtilities\Config\ConfigProvider;
  *
  * @throws \DanielDoyle\HappyUtilities\Exception\MissingConfigException
  * @throws \DanielDoyle\HappyUtilities\Exception\RegistryException
- * @param array $additionalConfigPaths Additional config paths
+ * @param string $scope                 Scope config
+ * @param array  $additionalConfigPaths Additional config paths
  * @return \DanielDoyle\HappyUtilities\Config\ConfigProvider
  */
-function config(array $additionalConfigPaths = [])
+function config(string $scope = '', array $additionalConfigPaths = [])
 {
     try {
-        return Registry::getConfigProvider();
+        $configProvider = Registry::getConfigProvider();
     } catch (RegistryException $exception) {
         $configProvider = new ConfigProvider($additionalConfigPaths);
 
         Registry::setConfigProvider($configProvider);
     }
 
-    return $configProvider;
+    return strlen($scope) ? $configProvider->get($scope) : $configProvider;
 }
